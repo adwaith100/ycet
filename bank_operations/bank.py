b7f3dc9050c705c474d92a1793445b337815d4d2
python <adwaithsnandhu2005@gmail.com>
2024-07-10 15:59:30 +0530

    second commit

--- bank_operations/bank.py
+++ bank_operations/bank.py
@@ -2,5 +2,5 @@ def credit(amount):
     return
 def debit(amount):
     return
-def transfer(amount,account_no):
-    return
+
+    
--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-from bank_operations.bank import credit,debit,transfer
+print("hello")
